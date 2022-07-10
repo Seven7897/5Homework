059b7216167426288bf01832b0ea6fae4006c513
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-10 18:10:35 +0300

    *First_three*

---
+++ homeWork003/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] array  = { 3 ,7 , 22 , 2 , 78}; 
+int minimal = array[0];
+int maximal = array[1];
+if (array[0] < array[1]) 
+{
+    minimal = array[0];
+    maximal = array[1];
+}    
+else
+{
+    minimal = array[1];
+    maximal = array[0];
+}
+for (int i = 2 ; i < array.Length; i++)
+{
+    if (array[i] > maximal) 
+    {
+        maximal = array[i];
+    } 
+    if (array[i] < minimal)
+    {
+        minimal = array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine(" разница составляет : " + ( maximal - minimal ) );
