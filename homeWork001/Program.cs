b7fa7beea2450ba8a7dfaf7f193d98b6052968e5
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-10 13:44:21 +0300

    *firstOne*

--- homeWork001/Program.cs
+++ homeWork001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество трехзначных чисел");
+int number = Convert.ToInt32(Console.ReadLine());
+int [] array = new int[number] ;
+int length = array.Length;
+int count = 0;
+Console.Write("[");
+for (int i = 0; i < length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write(array[i] + " ");
+    if ( array[i] % 2 == 0)
+    {
+        count += + 1;
+    }
+}
+Console.Write("]");
+Console.WriteLine();
+Console.WriteLine($"количество четных чисел равно : {count}");
