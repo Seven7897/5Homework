0c10702fc716d25498dba5f10031e15451249588
Anatoly Finchenko <finchenko777@gmail.com>
2022-07-10 17:55:51 +0300

    *First*

---
+++ homeWork002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("задайте длину массива : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0 ;
+int [] array = new int [number];
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100 , 101);
+    Console.Write(array[i] + " ");
+    if ( i % 2 != 0)
+        {
+            result += array[i];
+        }
+}
+Console.Write("]");
+Console.WriteLine("result = " + result );
